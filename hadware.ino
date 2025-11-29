084c455b96533d35746d9e637e5efac52e91fea7
Diego Cesar Salcedo Zerpa <diegocsalcedoz@gmail.com>
2025-11-29 00:47:26 -0500

    arduino archivo

---
+++ hadware.ino
@@ -0,0 +1,62 @@
+// Librerias I2C para controlar el mpu6050
+// la libreria MPU6050.h necesita I2Cdev.h, I2Cdev.h necesita Wire.h
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+// La dirección del MPU6050 puede ser 0x68 o 0x69, dependiendo 
+// del estado de AD0. Si no se especifica, 0x68 estará implicito
+MPU6050 sensor;
+
+// Valores RAW (sin procesar) del acelerometro y giroscopio en los ejes x,y,z
+int ax, ay, az;
+int gx, gy, gz;
+
+long tiempo_prev;
+float dt;
+float ang_x, ang_y;
+float ang_x_prev, ang_y_prev;
+
+void setup() {
+  Serial.begin(57600);    //Iniciando puerto serial
+  Wire.begin();           //Iniciando I2C  
+  sensor.initialize();    //Iniciando el sensor
+
+  if (sensor.testConnection()) Serial.println("Sensor iniciado correctamente");
+  else Serial.println("Error al iniciar el sensor");
+}
+
+void loop() {
+  // Leer las aceleraciones y velocidades angulares
+  sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz);
+  
+  // Necesito un nombre para esto ¿variación del tiempo?
+  dt = (millis()-tiempo_prev)/1000.0;
+  tiempo_prev=millis();
+  
+  //Calcular los ángulos con acelerometro
+  float accel_ang_x=atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
+  float accel_ang_y=atan(-ax/sqrt(pow(ay,2) + pow(az,2)))*(180.0/3.14);
+  
+  //Calcular angulo de rotación con giroscopio y filtro complemento  
+  ang_x = 0.98*(ang_x_prev+(gx/131)*dt) + 0.02*accel_ang_x;
+  ang_y = 0.98*(ang_y_prev+(gy/131)*dt) + 0.02*accel_ang_y;
+  
+  // Reasignación
+  ang_x_prev=ang_x;
+  ang_y_prev=ang_y;
+
+  //Mostrar los angulos separadas por un [tab]
+
+  //Serial.print("Rotacion en X:  ");
+  //Serial.print(ang_x+2.80); 
+  //Serial.print("tRotacion en Y: ");
+  //Serial.println(ang_y+7.10);
+  //Serial.println(sqrt(pow(ang_x+2.80, 2) + pow(ang_y+7.10, 2)));
+
+  // Función Arcotangente de dos parámetros y luego convertido en grados para ir de -180 a 180 grados
+  Serial.println(atan2(ay+2.80, az+7.10) * (180.0 / PI));
+
+  delay(10);
+}
